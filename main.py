e59db04fc9f0e1aee579ca7695f1a08a27b6edea
CodingalUsername <vannsh06@gmail.com>
2025-02-15 14:45:51 +0530

    init.commit

--- main.py
+++ main.py
@@ -9,16 +9,13 @@ def main():
         return
     print("Original image loaded successfully.")
     
-    sizes = {
- 'small' (200, 200),
-'medium' (400, 400),
-'large' (600, 600)}
+    sizes = {'small': (200, 200), 'medium': (400, 400), 'large': (600, 600)}
 
     for size_name, dimensions in sizes.items():
         resized = cv2.resize(image, dimensions)
         cv2.imshow(f"{size_name.capitalize()} Image", resized)
         cv2.imwrite(f"input_image_{size_name}.jpg", resized)
-        print(f"Image resized to (dimensions [0] x{dimensions [1]} pixels ({size_name}) and saved.")
+        print(f"Image resized to ({dimensions[0]}x{dimensions[1]} pixels ({size_name}) and saved.")
 
 
     print("Displaying resized images. Press any key in the image windows to exit.")
